cff3cccd97e2e32e71a88879bbb99f1ac1ca980d
Simon Vetter <simonvetter@gmx.net>
2012-03-02 23:08:57 +0100

    Second iteration "Fill the stroke simplification with code."
    The previous patch had a wrong relations character. Sorry for the mess ...
    
    Signed-off-by: Simon Vetter <simonvetter@gmx.net>
    Signed-off-by: Fabian Henze <flyser42@gmx.de>

--- xoj2tikz/optimizations.py
+++ xoj2tikz/optimizations.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of xoj2tikz.
-# Copyright (C) 2012 Fabian Henze
+# Copyright (C) 2012 Fabian Henze, Simon Vetter
 # 
 # xoj2tikz is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 # along with xoj2tikz.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+from math import sqrt
 
 from .page import Page
 from .layer import Layer
@@ -38,6 +39,36 @@ def detectRectangle(stroke):
     return stroke
 
 def simplifyStrokes(stroke):
+    """
+    Detect collinear parts of a stroke and remove them.
+    """
+    if isinstance(stroke, Stroke):
+        s = 0
+        while s < len(stroke.coordList) - 2:
+            ax = stroke.coordList[s][0]
+            ay = stroke.coordList[s][1]
+            bx = stroke.coordList[s+1][0]
+            by = stroke.coordList[s+1][1]
+            cx = stroke.coordList[s+2][0]
+            cy = stroke.coordList[s+2][1]
+            
+            # Calculate the dot / scalar product of the two vectors
+            scalarProduct = abs((ax-bx) * (bx-cx) + (ay-by) * (by-cy))
+            
+            # Calculate the lengths of both vectors (from a to b, from b to c)
+            firstLength = sqrt((ax-bx)**2 + (ay-by)**2)
+            secondLength = sqrt((bx-cx)**2 + (by-cy)**2)
+            
+            # If the product of both individual lengths is 'almost equal' to
+            # the scalar product, then these vectors are colinear.
+            # 0.99999 is an epsilon to compensate float inaccurracy.
+            # Testing has shown that this is a good value. maybe one should
+            # calculate the absolute error ...
+            if (firstLength * secondLength * 0.99999 < scalarProduct and
+                    len(stroke.coordList[s+1]) == 2):
+                del stroke.coordList[s+1]
+                continue
+            s += 1
     return stroke
 
 def runAll(document):
